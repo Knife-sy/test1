49e7a50c12864f0308ecab5cd3928185a5100ae3
Knife-sy <13250709552@163.com>
2018-06-27 13:34:49 +0000

    Move user model to models folder

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="jumnotron">
+<div class="jumbotron">
     <h1>Hello Laravel</h1>
     <p class="lead">
         你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">Laravel 入门教程</a> 的示例项目主页。
@@ -10,6 +10,6 @@
         一切，将从这里开始。
     </p>
     <p>
-        <a clas="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
     </p>
 @stop
